91aeb6bba49fc5da98c8e5f8d08bc6af7e8be5bb
Bercan aydin <143941941+maymun1234@users.noreply.github.com>
2025-10-10 14:35:16 +0300

    Update Bot2.py

--- Bot2.py
+++ Bot2.py
@@ -5,7 +5,7 @@ API_KEY = "6f81a9c5c8e6b92d01f89a33d6b5a7d3c9eacb90dcd33c0b89b5cfa22bffb510"
 
 
 payload = {
-    "content": "Merhaba! Bu bir bot tarafından gönderilen MiniBlog testi. Kısa ve öz.",
+    "content": "Github bot",
     "is_miniblog": True,
     # "author_username": "botuser"   # opsiyonel
 }
