e0d7da8005a88b0ccc2290c97a97b02104bdc59c
Bercan aydin <143941941+maymun1234@users.noreply.github.com>
2025-10-10 14:41:44 +0300

    Update Bot2.py

--- Bot2.py
+++ Bot2.py
@@ -1,1 +1,20 @@
-print("sdfvfvf")
+import requests
+
+API_URL = "https://bercan.blog/pages/api/minipost_create.php"
+API_KEY = "6f81a9c5c8e6b92d01f89a33d6b5a7d3c9eacb90dcd33c0b89b5cfa22bffb510"
+
+
+payload = {
+    "content": "Github bot",
+    "is_miniblog": True,
+    # "author_username": "botuser"   # opsiyonel
+}
+
+headers = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+}
+
+r = requests.post(API_URL, json=payload, headers=headers, timeout=10)
+print("Status:", r.status_code)
+print("Response:", r.text)
